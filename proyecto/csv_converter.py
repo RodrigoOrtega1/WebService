d1e43d830fddf220b48a77508eb498f6c7ef19d0
RodrigoOrtega1 <rodrigoortega@ciencias.unam.mx>
2022-03-09 19:52:12 -0600

    Implementacion de csv_converter usando pandas

--- proyecto/csv_converter.py
+++ proyecto/csv_converter.py
@@ -1,6 +1,12 @@
 import csv
+import pandas as pd
 
 # Clase que implementa un convertidor de archivo csv a diccionario de python
+
+# Convierte un csv a DataFrame de la libreria 'pandas'
+def to_df(doc_name):
+    df = pd.read_csv(doc_name)
+    return df
 
 # Convierte un documento csv a diccionario de python
 def to_dic(doc_name, row1, row2):
@@ -10,12 +16,16 @@ def to_dic(doc_name, row1, row2):
         dic = {rows[row1]:rows[row2] for rows in reader}
     return dic
 
-# origin = to_dic("dataset1.csv", 2, 3)
-# del origin['origin_latitude']
-# print(len(origin))
-# print(origin)
-#
-# destination = to_dic("dataset1.csv", 4, 5)
-# del destination['destination_latitude']
-# print(len(destination))
-# print(destination)
+data = to_df('dataset1.csv')
+data['Origen'] = data['origin_latitude'].astype(str) + ',' + data['origin_longitude'].astype(str)
+data['Destino'] = data['destination_latitude'].astype(str) + ',' + data['destination_longitude'].astype(str)
+del data['origin_latitude']
+del data['origin_longitude']
+del data['destination_latitude']
+del data['destination_longitude']
+print(data)
+a = data.drop_duplicates()
+print(a)
+a.to_csv('hola.csv')
+# diccionario = data.to_dict()
+# print(diccionario)
