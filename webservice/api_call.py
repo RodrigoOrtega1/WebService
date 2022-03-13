e02cd208699e0d06226d825f22a2b2cd8ce70f5b
RodrigoOrtega1 <rodrigoortega@ciencias.unam.mx>
2022-03-12 21:31:05 -0600

    Cambio a la documentación

--- webservice/api_call.py
+++ webservice/api_call.py
@@ -1,39 +1,35 @@
 import requests
 
-def check_str(str):
-    """
-    Toma una cadena y ve si esta compuesta de numeros
-    Params:
-        str    - La cadena a checar
-    Returns:
-        result - True si si esta compuesta de numeros
-    """
+def es_num(str):
+    '''
+    Toma una cadena
+    Regresa True si esta compuesta solamente de numeros
+    :param str: Una cadena
+    :return result: True si str esta compuesta de numeros
+    '''
     ls = str.lstrip("-").split(".")
-    result = all(n.isdigit() for n in ls) and len(ls) <= 2
-    return result
+    return all(n.isdigit() for n in ls) and len(ls) <= 2
 
 def peticion(lat, lon):
-    """
-    Pide una latitud y longitud y regresa la respuesta del API de openweathermap
-    Params:
-        lat    - La latitud
-        lon    - La longitud
-    Returns:
-        Una cadena con el nombre, clima y temperatura de las coordenadas
-    """
+    '''
+    Toma dos coordenadas
+    Regresa el clima en las coordenadas
+    :param lat: Una cadena con la latitud
+    :param lon: Una cadena con la longitud
+    :return: Una cadena con el clima en las coordenadas dadas
+    '''
     if type(lat) is not str or type(lon) is not str:
-        raise ValueError("Latitud y longitud deben ser cadenas de numeros")
-    if check_str(lat) == False or check_str(lon) == False:
-        raise ValueError("Latitud y longitud deben ser cadenas de numeros")
+        raise ValueError("Latitud y longitud deben ser cadenas exclusivamente de numeros")
+    if es_num(lat) == False or es_num(lon) == False:
+        raise ValueError("Latitud y longitud deben ser cadenas exclusivamente de numeros")
 
-    llave = "" #Colocar una key al API de openweathermap
+    llave = "" #Colocar llave de OpenWeatherMap
     if (llave and not llave.isspace()):
         url = f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={llave}&units=metric"
     else:
-        raise ValueError("El programa debe tener una llave del API de openweathermap")
+        raise ValueError("El programa debe tener una llave de OpenWeatherMap valida")
 
     respuesta = requests.get(url).json()
-
     nombre = respuesta['name']
     clima = respuesta['weather'][0]['main']
     temperatura = respuesta['main']['temp']
--- webservice/csv_converter.py
+++ webservice/csv_converter.py
@@ -1,12 +1,10 @@
 import pandas as pd
 
-def to_df(doc_name):
-    """
-    Convierte un csv a un DataFrame
-    Params:
-        doc_name    - Nombre del documento csv
-    Returns:
-        df          - Un DataFrame del documento dado
-    """
-    df = pd.read_csv(doc_name)
-    return df
+def a_df(doc):
+    '''
+    Toma un archivo .csv
+    Regresa un DataFrame
+    :param doc: Nombre del documento csv
+    :return: Un DataFrame de doc
+    '''
+    return pd.read_csv(doc)
